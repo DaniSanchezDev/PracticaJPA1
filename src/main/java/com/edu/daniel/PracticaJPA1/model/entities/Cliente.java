d694bce636ea45cb9f4b9ac80611165e59641fbb
imdaano <danisanchezpsc@gmail.com>
2024-08-19 08:58:02 +0200

    create class cliente

--- src/main/java/com/edu/daniel/PracticaJPA1/model/entities/Cliente.java
+++ src/main/java/com/edu/daniel/PracticaJPA1/model/entities/Cliente.java
@@ -32,4 +32,101 @@ public class Cliente {
     @Column(name = "categoria", columnDefinition = "ENUM('VIP', 'REGULAR', 'INVITADO','CASUAL')")
     private Categoria categoria;
 
+
+    public Cliente() {
+
+    }
+
+
+    public Cliente(Long idCliente, String nombre, String apellido1, String apellido2, LocalDate fechaNacimiento,
+            int telefono, Categoria categoria) {
+        this.idCliente = idCliente;
+        this.nombre = nombre;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+        this.fechaNacimiento = fechaNacimiento;
+        this.telefono = telefono;
+        this.categoria = categoria;
+    }
+
+
+    public Long getIdCliente() {
+        return idCliente;
+    }
+
+
+    public void setIdCliente(Long idCliente) {
+        this.idCliente = idCliente;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Cliente [idCliente=" + idCliente + ", nombre=" + nombre + ", apellido1=" + apellido1 + ", apellido2="
+                + apellido2 + ", fechaNacimiento=" + fechaNacimiento + ", telefono=" + telefono + ", categoria="
+                + categoria + "]";
+    }
+
+    
+    
 }
