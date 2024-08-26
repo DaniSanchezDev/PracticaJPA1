547ee17263f66301af7a202dd4b0b4dab79090df
imdaano <danisanchezpsc@gmail.com>
2024-08-26 08:53:56 +0200

    add new Spring Code

--- src/main/java/com/edu/daniel/PracticaJPA1/model/entities/Staff.java
+++ src/main/java/com/edu/daniel/PracticaJPA1/model/entities/Staff.java
@@ -1,12 +1,19 @@
 package com.edu.daniel.PracticaJPA1.model.entities;
 import java.time.*;
 
+import com.edu.daniel.PracticaJPA1.model.enumerated.Cargo;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 public class Staff {
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idStaff;
     private String nombre;
     private String apellido1;
@@ -14,6 +21,9 @@ public class Staff {
     private String apellido2;
     private LocalDate fechaNacimiento;
     private int telefono;
-    
+    @Enumerated(EnumType.STRING)
+    @Column(name = "cargo", columnDefinition = "ENUM('GERENTE', 'ENCARGADO', 'SEGURIDAD','CAMARERO', 'LIMPIEZA', 'RUNNER')")
+    private Cargo cargo;
+
 
 }
