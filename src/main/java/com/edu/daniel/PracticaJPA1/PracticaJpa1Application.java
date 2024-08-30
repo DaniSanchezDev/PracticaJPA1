88a1a4b54d4e4eb45748497b9ba28fb67a1bb7ba
imdaano <danisanchezpsc@gmail.com>
2024-08-30 08:39:47 +0200

    add new Spring Code

--- src/main/java/com/edu/daniel/PracticaJPA1/PracticaJpa1Application.java
+++ src/main/java/com/edu/daniel/PracticaJPA1/PracticaJpa1Application.java
@@ -11,12 +11,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.edu.daniel.PracticaJPA1.model.entities.Cliente;
 import com.edu.daniel.PracticaJPA1.model.enumerated.Categoria;
 import com.edu.daniel.PracticaJPA1.repositories.IClienteRepository;
+import com.edu.daniel.PracticaJPA1.repositories.ILocalRepository;
 
 @SpringBootApplication
 public class PracticaJpa1Application implements CommandLineRunner{
 
 	@Autowired
 	private IClienteRepository clienteRepo;
+
+	@Autowired
+	private ILocalRepository localRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PracticaJpa1Application.class, args);
@@ -31,6 +35,8 @@ public class PracticaJpa1Application implements CommandLineRunner{
 	// System.out.println(clienteRepo.findByCategoria(Categoria.VIP));
 	// 2º Buscar por categoria VIP y mayores de 20
 	// System.out.println("VIPs con más de 20 años: " + clienteRepo.findByCategoriaAndClienteGreaterThan(Categoria.VIP, LocalDate.of(2014, 12, 31)));
+	// 3ºBuscar locales de aforo entre 50 y 100
+	System.out.println(localRepo.findByAforoMaxBetween(50, 100));
 
 	
 	}
